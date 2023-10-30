6eef7e6268b2e0c957c7c77aaab716bf98c87ef2
baby satheeshkumar <baby1392@gmail.com>
2023-10-30 12:15:11 +0530

    Add files via upload

---
+++ src/main/java/webapp/LoginServlet.java
@@ -0,0 +1,34 @@
+package webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+//		PrintWriter out = response.getWriter();
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("<title>Servlet!!!</title>");
+//		out.println("</head>");
+//		out.println("<body>");
+//		out.println("My First Servlet!!!");
+//		out.println("</body>");
+//		out.println("</html>");
+		
+		request.setAttribute("name", request.getParameter("name"));
+		
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,17 @@
+<%-- <%@ page language="java" contentType="text/html; charset=ISO-8859-1" --%>
+<%--     pageEncoding="ISO-8859-1"%> --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="java.lang.*" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login Page</title>
+</head>
+<%
+%>
+<body>
+ <h1> Hello ${ name } !!!</h1>
+</body>
+</html>
